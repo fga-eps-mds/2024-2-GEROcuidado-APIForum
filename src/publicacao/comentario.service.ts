e18205ad78873bcce28f900bda6c98fffcb38955
Fae <gabrielsampaio.fae@gmail.com>
2025-02-09 16:10:49 -0300

    Reajustando pare receber o id correto no list

--- src/publicacao/comentario.service.ts
+++ src/publicacao/comentario.service.ts
@@ -3,15 +3,13 @@ import { ClientProxy } from '@nestjs/microservices';
 import { InjectRepository } from '@nestjs/typeorm';
 import { lastValueFrom, timeout } from 'rxjs';
 import { Repository } from 'typeorm';
+import { Publicacao } from '../publicacao/entities/publicacao.entity';
 import { Ordering } from '../shared/decorators/ordenate.decorator';
 import { Pagination } from '../shared/decorators/paginate.decorator';
 import { ResponsePaginate } from '../shared/interfaces/response-paginate.interface';
 import { CreateComentarioDto } from './dto/create-comentario.dto';
 import { UpdateComentarioDto } from './dto/update-comentario.dto';
 import { Comentario } from './entities/comentario.entity';
-import {
-  IPublicacaoFilter
-} from './interface/publicacao-filter.interface';
 import {
   IUsuario,
 } from './interface/publicacao-usuario.interface';
@@ -25,12 +23,22 @@ export class ComentariosService {
   ) { }
 
   async create(createComentarioDto: CreateComentarioDto): Promise<Comentario> {
+    // Carregar a Publicacao com o ID fornecido
+    const publicacao = await this.comentarioRepository.manager.findOne(Publicacao, { where: { id: createComentarioDto.publicacaoId } });
+
+    if (!publicacao) {
+      throw new NotFoundException(`Publicação com ID ${createComentarioDto.publicacaoId} não encontrada`);
+    }
+
+    // Criar o comentário com a publicação associada
     const comentario = this.comentarioRepository.create({
-      ...createComentarioDto,
+      ...createComentarioDto,  // Preenche os dados do DTO
+      publicacao,               // Associa a publicação encontrada ao comentário
     });
 
-    return await this.comentarioRepository.save(comentario);
+    return await this.comentarioRepository.save(comentario); // Salva o comentário com a associação
   }
+
 
 
   async findAll(
@@ -61,8 +69,13 @@ export class ComentariosService {
   async findOne(id: number): Promise<Comentario> {
     const comentario = await this.comentarioRepository.findOneOrFail({
       where: { id },
-      relations: ['publicacao', 'usuario'] // Aqui garantimos que a publicacao será carregada
     });
+
+    const publicacao = await this.comentarioRepository.manager.findOne(Publicacao, { where: { id: comentario.publicacao.id } });
+
+    if (!publicacao) {
+      throw new NotFoundException(`Publicação com ID ${id} não encontrada`);
+    }
 
     const request = this.clientProxy
       .send({ role: 'info', cmd: 'get' }, { id: comentario.idUsuario })
@@ -72,7 +85,7 @@ export class ComentariosService {
     if (!comentario) {
       throw new NotFoundException(`Comentário com ID ${id} não encontrado`);
     }
-    const comentarioWithUsuario = { ...comentario, usuario, publicacao: comentario.publicacao } as Comentario & { usuario: IUsuario } & { publicacao: IPublicacaoFilter };
+    const comentarioWithUsuario = { ...comentario, usuario, publicacao } as Comentario & { usuario: IUsuario };
     return comentarioWithUsuario;
   }
 
